2e2c075fd32430fe2525b4d35357da32eb20378f
kaoutarmim <74470495+kaoutarmim@users.noreply.github.com>
2021-04-15 15:09:40 +0000

    Update database.py

--- database.py
+++ database.py
@@ -23,3 +23,32 @@ try :
                                     database='postgres')
 except :
     print("Erreur lors de la connexion !")
+cur = connection.cursor()
+sql = '''Create database DATAcollection; '''
+    
+cur.execute(sql)
+connection.commit()
+sql = '''CREATE TABLE books (
+             title VARCHAR(100) NOT NULL PRIMARY KEY,
+             author VARCHAR(40) NOT NULL,
+             statut VARCHAR(20) NOT NULL,
+             déposé_par SMALLINT NOT NULL,
+             emprunté_par SMALLINT NOT NULL,
+             category VARCHAR(50) ,
+             lend_date datetime 
+              
+); '''
+    
+cur.execute(sql)
+connection.commit()
+sql = '''CREATE TABLE Infos(
+             title VARCHAR(100) NOT NULL,
+             Email VARCHAR(100) NOT NULL,
+             b_Description TEXT,
+             Rating SMALLINT,
+             PRIMARY KEY(title)
+)
+; '''
+    
+cur.execute(sql)
+connection.commit()
